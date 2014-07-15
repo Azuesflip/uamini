d51e9e80045b13c30b051a2619df4b8496104927
Bob Kupfner <azuesflip@gmail.com>
2014-07-15 15:18:32 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uamini::Application.config.secret_key_base = '8df3099821be64978998d08811f15bdd687f9edd8a1ef9bf3b2736369a9925f506aa145693368354e8bc5dc8929cd6ce7fca7bc697e1e38e8550dc1dcae2b2e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Uamini::Application.config.session_store :cookie_store, key: '_uamini_session'
